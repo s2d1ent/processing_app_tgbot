3037c95f87da0769cd04067162545f556824a8a8
s2d1ent <vornfrost@gmail.com>
2024-10-21 21:28:39 +0300

    add LICENSE

--- src/config.py
+++ src/config.py
@@ -1,14 +1,34 @@
+# MIT License
+# 
+# Copyright (c) 2024 Viktor Tyumenev
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# 
+
 import telebot
 from telebot import types
 from telebot.types import InputMediaPhoto, InputMediaVideo, InputMediaAudio, InputMediaDocument
 from datetime import datetime
-#db_name = 'DATABASE_NAME'
-#dmin_key = 'ADMIN_KEY'
-#suadmin_key = 'DONT_WORK_NOW'
-#token = "TELEGRAM_BOT_TOKEN"
-db_name = 'mponn_it_test'
-admin_key = 'Qwerty'
-token = "7734632816:AAFdTBHB2BkTpsh4l9zd7vchvLJCvrBWqF4"
+db_name = 'DATABASE_NAME'
+dmin_key = 'ADMIN_KEY'
+suadmin_key = 'DONT_WORK_NOW'
+token = "TELEGRAM_BOT_TOKEN"
 buffer = {}
 user_status = {}
 user_questions_buf = {}
--- src/db_configur.py
+++ src/db_configur.py
@@ -1,3 +1,26 @@
+# MIT License
+# 
+# Copyright (c) 2024 Viktor Tyumenev
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# 
+
 import sqlite3
 from config import db_name
 import sys
@@ -14,19 +37,10 @@ def execute(database):
     
     try:
         with open(database, 'r', encoding='utf-8') as file:
-            #file = file.read()
-            #sql_commands = file.split(";")
-            #for cmd in sql_commands:
-            #    cmd = cmd.strip()
-            #    if cmd:
-            #        print(cmd)
-            #        cursor.execute(cmd)
             with open(arg, 'r') as f:
                 sql_script = f.read()
                 cursor.executescript(sql_script)
         print(f"The dump {database} import done!")
-    #except Exception as e:
-    #    print(f"{database} "+repr(e))
     except sqlite3.Error as e:
         print(f"An error occurred: {e}")
 
